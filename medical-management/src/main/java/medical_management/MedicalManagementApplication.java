fafe02d68edbf9790b2c7d20378dd22af32fd95b
harshad surse <158550410+harsh7516@users.noreply.github.com>
2026-04-09 19:41:08 +0530

    Day 1 - Initial Spring Boot setup

---
+++ medical-management/src/main/java/medical_management/MedicalManagementApplication.java
@@ -0,0 +1,13 @@
+package medical_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalManagementApplication.class, args);
+	}
+
+}
---
+++ medical-management/src/test/java/medical_management/MedicalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package medical_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
